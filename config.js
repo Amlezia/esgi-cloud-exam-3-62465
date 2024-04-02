72354f4763b33ed9f2c21e855d4fc15115b1af5b
Amlezia <alia.mezouar2@gmail.com>
2024-04-02 12:29:25 +0200

    initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: 'K4JC5AMXR0XDP1WMERYC', // TODO
+    secretAccessKey: 'I4d1dgWeWgpPVJ5f5zebCn8UZ8z1zzyPo281ozKe', // TODO
+    endpoint: 's3.eu-central-2.wasabisys.com', // TODO
+    bucket: 'esgi-cloud-exam-bucket-62465', // TODO
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
